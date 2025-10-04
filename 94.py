31c811b739f040da615eb0354215d7c79cdca881
EDCAKDC <georgehealer233@gmail.com>
2025-10-04 00:47:02 -0400

    Add files via upload

--- 94.py
+++ 94.py
@@ -4,6 +4,9 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from typing import List, Optional
+
+
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res = []
@@ -34,6 +37,36 @@ class Solution:
                 stack.append(cur)
                 cur = cur.left
             cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+        return res
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        cur = root
+        while cur:
+            if not cur.left:
+                res.append(cur.val)
+                cur = cur.right
+                continue
+            mostright = cur.left
+            while mostright.right and mostright.right is not cur:
+                mostright = mostright.right
+            if not mostright.right:
+                mostright.right = cur
+                cur = cur.left
+                continue
+            else:
+                mostright.right = None
             res.append(cur.val)
             cur = cur.right
         return res
