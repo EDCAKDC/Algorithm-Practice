513e292eeb2678963ceea717201c75a38cf7fb08
EDCAKDC <georgehealer233@gmail.com>
2025-11-09 22:37:16 -0500

    Add files via upload

---
+++ 526.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countArrangement(self, n: int) -> int:
+        options = [[] for _ in range(n+1)]
+        for pos in range(1, n+1):
+            for num in range(1, n+1):
+                if pos % num == 0 or num % pos == 0:
+                    options[pos].append(num)
+        used = [False] * (n+1)
+        ans = 0
+        order = list(range(1, n+1))
+        order.sort(key=lambda p: len(options[p]))
+
+        def dfs(k):
+            nonlocal ans
+            if k == n:
+                ans += 1
+                return
+            pos = order[k]
+            for num in options[pos]:
+                if not used[num]:
+                    used[num] = True
+                    dfs(k+1)
+                    used[num] = False
+        dfs(0)
+        return ans
